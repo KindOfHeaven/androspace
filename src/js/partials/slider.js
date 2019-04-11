5707ac5e7b67512cc734afc86cc2ba823f71b1b6
Кирилл Тен <kirillten@MacBook-Pro-Kirill.local>
2019-04-11 16:26:14 +0300

    Fix

--- src/js/partials/slider.js
+++ src/js/partials/slider.js
@@ -1,6 +1,6 @@
 
 function slider() {
-  if (jQuery(window).outerWidth() < 769) {
+  if (jQuery(window).outerWidth() < 768) {
     jQuery('.js-slider:not(.-large)').each(function() {
       if (jQuery(this).children('a:not(.button--all)').length) {
         jQuery(this).children('a:not(.button--all)').wrapAll('<div class="js-slider__init"></div>');
@@ -38,19 +38,7 @@ function slider() {
         ]
       });
     }
-  } else {
-    if (jQuery('.js-slider__init').length) {
-      jQuery('.js-slider__init').slick('unslick')
-      jQuery('.js-slider__init > a').unwrap()
-    }
-  }
-}
-
-slider();
-jQuery(window).resize(function() {
-  slider();
-});
-jQuery('.js-slider.-large').slick({
+    jQuery('.js-slider.-large').slick({
   responsive: [
     {
       breakpoint: 3000,
@@ -70,21 +58,33 @@ jQuery('.js-slider.-large').slick({
   ]
 });
 jQuery('#topSlider').slick({
-  	responsive: [
-    	{
-    	  breakpoint: 3000,
+    responsive: [
+      {
+        breakpoint: 3000,
         settings: "unslick"
-    	},
-    	{
-    	  breakpoint: 768,
-    	  settings: {
-    	    dots: true,
-  			infinite: false,
-  			speed: 300,
-  			arrows: false,
-  			slidesToShow: 1,
-    	    slidesToScroll: 1
-    	  }
-    	}
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          dots: true,
+        infinite: false,
+        speed: 300,
+        arrows: false,
+        slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
     ]
 })
+  } else {
+    if (jQuery('.js-slider__init').length) {
+      jQuery('.js-slider__init').slick('unslick')
+      jQuery('.js-slider__init > a').unwrap()
+    }
+  }
+}
+
+slider();
+jQuery(window).resize(function() {
+  slider();
+});
